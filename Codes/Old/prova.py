a8d4595bb82d2ad2bab4e928683907641892116e
Marco Cocciaretto <m.cocciaretto@protonmail.com>
2022-09-02 15:47:56 +0200

    Removed deprecated code

--- Codes/Old/prova.py
+++
@@ -1,7 +0,0 @@
-Directory = '/home/marco/Documents/University/Physics/MetodiNumerici/Modulo1/_data/'
-out_file = open(Directory+'prova.txt', "w+")
-
-a=[[1,2],[1,2,3,4]]
-
-out_file.write(str(a))
-out_file.close()
